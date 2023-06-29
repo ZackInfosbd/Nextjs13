c72acf166b170183fd02c931b3ef4e5579c26706
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2023-06-29 17:31:55 +0200

    ci: local images better approach

--- Reactjs18/fundamentals/src/data.js
+++ Reactjs18/fundamentals/src/data.js
@@ -1,20 +1,24 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+
 export const data = [
   {
     id: 1,
     title: 'beautiful life',
     author: 'kedour el merhi Ba3ata',
-    img: './images/book-1.jpg',
+    img: img1,
   },
   {
     id: 2,
     title: 'Ouzera',
     author: 'bou3lem hmida',
-    img: './images/book-2.jpg',
+    img: img2,
   },
   {
     id: 3,
     title: 'kedour el haycha',
     author: 'denideni',
-    img: './images/book-3.jpg',
+    img: img3,
   },
 ];
