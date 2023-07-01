8dae404784bf815f71f39db979312921e9d29c83
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2023-07-01 18:25:14 +0200

    ci: user chalenge

--- Reactjs18/hooks-advanced-react/README.md
+++ Reactjs18/hooks-advanced-react/README.md
@@ -1285,3 +1285,57 @@ Improvements
 ```js
 <button className='btn' onClick={() => setShowAlert(!showAlert)}>
 ```
+
+### User Challenge
+
+```js
+import Starter from './tutorial/03-conditional-rendering/starter/07-user-challenge.jsx';
+```
+
+- create state value
+  - user - default value null
+- create two functions
+  - login - set's user equal to object with name property
+  - logout - set's user equal to null
+- in jsx use ? to display two different setups
+
+- h4 with "hello there, user name" and logout button
+- h4 with "please login " and login button
+
+```js
+import { useState } from 'react';
+
+const UserChallenge = () => {
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    // normally connect to db or api
+    setUser({ name: 'vegan food truck' });
+  };
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h4>hello there, {user.name}</h4>
+          <button className="btn" onClick={logout}>
+            logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>Please Login</h4>
+          <button className="btn" onClick={login}>
+            login
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserChallenge;
+```
--- Reactjs18/hooks-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ Reactjs18/hooks-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,66 @@
+import { useState } from 'react';
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  const logIn = () => {
+    setUser({ name: 'zakaria' });
+  };
+
+  const logOut = () => {
+    setUser(null);
+  };
+
+  // return (
+  //   <>
+  //     {user ? (
+  //       <LoggedIn name={user.name} handleLogout={logOut} />
+  //     ) : (
+  //       <LoggedOut handleLogout={logIn} />
+  //     )}
+  //   </>
+  // );
+  return (
+    <>
+      {user ? (
+        <div>
+          <h4> hello {user.name} </h4>
+          <button className="btn" onClick={logOut}>
+            logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>Hello there, please login</h4>
+          <button className="btn" onClick={logIn}>
+            login
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+const LoggedIn = (name, handleLogout) => {
+  return (
+    <div>
+      <h4> hello {name} </h4>
+      <button className="btn" onClick={handleLogout}>
+        logout
+      </button>
+    </div>
+  );
+};
+
+const LoggedOut = (handleLogin) => {
+  return (
+    <div>
+      <h4>Hello there, please login</h4>
+      <button className="btn" onClick={handleLogin}>
+        login
+      </button>
+    </div>
+  );
 };
 
 export default UserChallenge;
