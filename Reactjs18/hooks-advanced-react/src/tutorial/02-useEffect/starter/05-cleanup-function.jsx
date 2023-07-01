9b63594bf0ae4edd7527e6dcba4d0fe26c29e6cc
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2023-07-01 19:55:01 +0200

    ci: clean up function/setup, timer example

--- Reactjs18/hooks-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ Reactjs18/hooks-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,32 @@
+import React, { useEffect } from 'react';
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = React.useState(false);
+
+  console.log('render');
+
+  return (
+    <>
+      <button type="button" className="btn" onClick={() => setToggle(!toggle)}>
+        Toggle component
+      </button>
+      {toggle && <RandomComponent />}
+    </>
+  );
+};
+
+const RandomComponent = () => {
+  useEffect(() => {
+    // console.log('hmm, this is interesting');
+    const inter = setInterval(() => {
+      // console.log('hello from interval');
+    }, 1000);
+    return () => {
+      clearInterval(inter);
+      console.log('clean up');
+    };
+  }, []);
+  return <h1>hello there</h1>;
 };
 
 export default CleanupFunction;
